365fd5ee3b48df933e5d93ab52d07e30af1d4896
JS SYA <syahirah96_shari@naver.com>
2018-05-09 15:39:28 +0900

    Update tutorials, join, start activity (layouts)

--- app/src/main/java/com/myfirstapp/logintest2/StartActivity.java
+++ app/src/main/java/com/myfirstapp/logintest2/StartActivity.java
@@ -2,15 +2,52 @@ package com.myfirstapp.logintest2;
 
 import android.*;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+
+//daily task, missions 보여주기
 public class StartActivity extends AppCompatActivity {
 
+    TextView txtPhone;
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_sign_out){
+            signOut();
+        }
+        return true;
+    }
+
+    private void signOut(){
+
+        AuthUI.getInstance().signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        startActivity(new Intent(StartActivity.this, MainActivity.class));
+                        finish();
+                    }
+                });
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +55,7 @@ public class StartActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolBar);
         toolbar.setTitle("Promise Together");
         setSupportActionBar(toolbar);
+
     }
 
 
